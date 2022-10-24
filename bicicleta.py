bdc4b319fc5b26660c5d207bafa8cfac273b7dc0
aleandresss <114179514+aleandresss@users.noreply.github.com>
2022-10-24 18:36:25 +0200

    pido que me imprima la bici

--- bicicleta.py
+++ bicicleta.py
@@ -3,4 +3,7 @@ from vehiculo import*
 class bicicleta (vehiculo):
     def __init__(self,color,ruedas,tipo):
         vehiculo.__init__(self,color,ruedas)
-        
+        self.tipo=tipo
+bici1=bicicleta("rojo",2,"urbana")
+print(bici1.tipo)
+
