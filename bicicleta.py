61bf3efdbc494f5622b251bdb696e9d7bb76b24f
aleandresss <114179514+aleandresss@users.noreply.github.com>
2022-10-24 18:35:00 +0200

    creo la clase con la funcion dentro

---
+++ bicicleta.py
@@ -0,0 +1,6 @@
+from vehiculo import*
+
+class bicicleta (vehiculo):
+    def __init__(self,color,ruedas,tipo):
+        vehiculo.__init__(self,color,ruedas)
+        
--- coche.py
+++ coche.py
@@ -3,6 +3,12 @@ from vehiculo import*
 class coche (vehiculo):
     def __init__(self,color,ruedas,velocidad,cilindrada):
         vehiculo.__init__(self,color,ruedas)
-        self
+        self.velocidad =velocidad
+        self.cilindrada =cilindrada
 
-        
+        ef __str__(self):
+        return vehiculo.__str__(self) + ", {} km/h, {} cc".format(self.velocidad,self.cilindrada)
+
+
+
+
