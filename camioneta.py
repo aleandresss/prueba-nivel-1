297a561e57a49660ad82513e6250aa67b1e0308b
aleandresss <114179514+aleandresss@users.noreply.github.com>
2022-10-24 18:40:04 +0200

    pongo super() para no nombrar la clase tal cual

--- camioneta.py
+++ camioneta.py
@@ -2,3 +2,7 @@ from vehiculo import *
 from coche import *
 
 class camioneta(coche):
+    def __init__ (self, color, ruedas, velocidad, cilindrada,carga):
+        super().__init__(color, ruedas, velocidad, cilindrada)
+        self.carga = carga 
+        
